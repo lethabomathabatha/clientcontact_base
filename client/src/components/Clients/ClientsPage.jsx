42a29c281a3d80b0c14efbd2b2323ad004713f70
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-13 22:37:55 +0200

    create Clients and Owners page text

---
+++ client/src/components/Clients/ClientsPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+export default function ClientsPage() {
+    
+    return (
+        <>
+            <div>These are your clients</div>
+        </>
+    )
+}
---
+++ client/src/components/Contacts/ContactsPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+export default function ContactsPage() {
+    
+    return (
+        <>
+            <div>These are your contacts</div>
+        </>
+    )
+}
