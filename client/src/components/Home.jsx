07e1c4d0fa46298ba4a8450e11a5bba839f3afe1
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-13 22:03:57 +0200

    create app components

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Home() {
+    
+    return (
+        <div>Welcome to your Client - Contact Base</div>
+    )
+}
---
+++ client/src/components/TestAPI.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function TestAPI() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchAPI = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/');
+      setData(response.data.fruits);
+      console.log(response.data.fruits);
+    } catch (error) {
+      setError('Failed to fetch data');
+    }
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <div>
+      <h1>API Test</h1>
+      {data.length > 0 ? (
+        <div>
+          <h2>Data from Server:</h2>
+          {data.map((fruit, index) => (
+            <div key={index}>
+              <p>{fruit}</p>
+              <br />
+            </div>
+          ))}
+        </div>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
