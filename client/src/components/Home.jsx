312342bc5f65807a52d960bc2ae2e081a361144a
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-14 17:09:45 +0200

    add custom page styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,18 +3,40 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import './../App.css'; 
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 export default function Home() {
-    
     return (
-        <>
-            <div>Welcome to your Client - Contact Base</div>
+      <Container fluid className="hero-section text-white">
+
+        
+        {/* Main Header Content */}
+        <Row className="d-flex justify-content-center align-items-center min-vh-100 text-center">
+          <Col md={6} className="px-2">
+            <h1>CC Base Efficiently Organizes Your Client and Contact Information</h1>
+            <p className="lead">I want to see...</p>
+
             <Link to="/clients">
-                <button>Clients</button>
+            <Button variant="warning" className="ms-2">Clients</Button>
             </Link>
             <Link to="/contacts">
-                <button>Contacts</button>
+            <Button variant="warning" className="ms-2">Contacts</Button>
             </Link>
-        </>
-    )
-}
+          </Col>
+        </Row>
+  
+       
+      </Container>
+    );
+  }
+  
+
+  
+
+{/* <Link to="/clients">
+<button className="custom-red-btn">Clients</button>
+</Link>
+<Link to="/contacts">
+<button>Contacts</button>
+</Link> */}
