d7dc2613568ae8a83d800f021ebc0afcf72e5f2b
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-14 10:05:06 +0200

    add api path and form validation

--- client/src/components/Clients/CreateClient.jsx
+++ client/src/components/Clients/CreateClient.jsx
@@ -2,26 +2,46 @@ import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Button } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function CreateClient() {
     const [client_name, setClientName] = useState('');
+    const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleClientSubmit = (e) => {
         e.preventDefault();
-        console.log(client_name);
+
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        setValidated(true)
+
+        axios.post('/create_client', client_name)
+        .then((res) => {
+            navigate('/clients')
+            console.log(result);   
+        })
+        .catch((err) => console.log(err))
+        
     }
     
     return (
         <>
             <div className='text-center'>
                 <div className='fw-bold'>Create Client Form</div>
+                <Link to='/' className='btn btn-success'>Back to Home</Link>
 
-                <form action="" onSubmit={handleSubmit} method="">
-                    <label>
-                        Client Name:
-                        <input type="text" value={client_name} onChange={(e) => setClientName(e.target.value)} />
-                    </label>
+                <form action="" onSubmit={handleClientSubmit} noValidate validated={validated} method="">
+                    <div class="form-floatin">
+                        <label for="client_name">Client Name</label>
+                        <input 
+                            type="text" class="form-control" id="client_name" placeholder="Client Name" name='client_name' required
+                            value={client_name} onChange={(e) => setClientName(e.target.value)}  
+                        />
+                    
+                    </div>
                     <button type="submit">Create Client</button>  
                 </form>  
             </div>
