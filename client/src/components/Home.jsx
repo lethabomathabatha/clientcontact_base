aa3b3ac8291b1a91df6c861d9da9533f2ad6bd73
Lethabo Mathabatha <118384529+lethabomathabatha@users.noreply.github.com>
2024-10-15 21:06:13 +0200

    adjust page layout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,23 +10,23 @@ import { FiMenu } from 'react-icons/fi'
 export default function Home() {
     return (
       <div fluid className="hero-section text-white">
-        <div className="d-flex flex-row justify-space-between position-top">
-        <p>Logo</p>
-        <FiMenu />
-        </div>
-      
+
         {/* Main Header Content */}
         <div className="d-flex justify-content-center align-items-center text-center">
           <div className="px-2 w-75">
-            <h1>CC Base Efficiently Organizes Your Client and Contact Information</h1>
+            <h1 className='fw-light'>Client Contact Base Efficiently Organizes Your <strong className="fw-bold">Client and Contact Information</strong></h1>
+            
+            <br />
             <p className="lead">I want to see...</p>
 
-            <Link to="/clients">
-            <Button variant="warning" className="home-btn ms-2 fs-5 px-4">Clients</Button>
-            </Link>
-            <Link to="/contacts">
-            <Button variant="warning" className="home-btn ms-2 fs-5 px-4">Contacts</Button>
-            </Link>
+            <div className='home-btn-container'>
+              <Link to="/clients">
+                <Button variant="warning" className="home-btn ms-2 fs-5 px-4">Clients</Button>
+              </Link>
+              <Link to="/contacts">
+                <Button variant="warning" className="home-btn ms-2 fs-5 px-4">Contacts</Button>
+              </Link>
+            </div>
           </div>
         </div>
   
@@ -34,9 +34,6 @@ export default function Home() {
       </div>
     );
   }
-  
-
-  
 
 {/* <Link to="/clients">
 <button className="custom-red-btn">Clients</button>
