159724f20a9ca3f12141819be5172ed7caee3781
Lethabo Mathabatha <118384529+lethabomathabatha@users.noreply.github.com>
2024-10-15 23:17:08 +0200

    add success page styling

--- client/src/components/Clients/ClientSuccess.jsx
+++ client/src/components/Clients/ClientSuccess.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
 export default function ClientSuccess() {
     
     return (
-        <>
-            <div>Client Successfully created!</div>
-            <Link to="/clients">
-                <button>View Clients</button>
-            </Link>
-        </>
+        <div className="success-container d-flex flex-column align-items-center justify-content-center min-vh-100">
+            <div className="success-message text-center">
+                <h1 className="fw-bold fs-3">Success!</h1>
+                <p className="lead">Contact successfully created!</p>
+                <Link to="/contacts" className="mt-2">
+                    <Button className='back-btn'>
+                        View Clients
+                    </Button>
+                </Link>
+            </div>
+        </div>
     )
 }
