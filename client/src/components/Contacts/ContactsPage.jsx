ea0dcddd9bfd96d8079507d302b4027e24c12a46
Lethabo Mathabatha <118384529+lethabomathabatha@users.noreply.github.com>
2024-10-15 21:33:35 +0200

    update page layout

--- client/src/components/Contacts/ContactsPage.jsx
+++ client/src/components/Contacts/ContactsPage.jsx
@@ -3,24 +3,28 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import ContactsTable from './ContactsTable';
+import { FiChevronLeft } from "react-icons/fi";
 
 import { Link } from 'react-router-dom';
 
 export default function ContactsPage() {
     
     return (
-        <div className="text-white min-vh-100 ">
-            <Link to='/' className='btn btn-success'>Back to Home</Link>
+        <div className="text-white m-5 mx-auto justify-content-center d-flex flex-column align-items-center">
+            <Link to='/' className=' back-btn text-decoration-none text-white'>
+                <FiChevronLeft />
+                Back to Home
+            </Link>
             
-                <div className='fw-bold'>Contacts</div>
-
-                {/* search bar to search existing clients from table */}
-                {/* <input type="text" placeholder="Search contacts..." /> */}
-                <br />
+            <div class="custom-header-section d-flex justify-content-between align-items-center">
+                <p class="custom-header text-lowercase fw-bold">Contacts</p>
+           
                 <Link to="/contacts/new">
-                    <button>Create New Contact</button>
+                    <button class="custom-create-btn fw-light px-4 ">+ Create New Contact</button>
                 </Link>
-                <ContactsTable />
+            </div>
+
+            <ContactsTable />
         </div>
     )
 }
