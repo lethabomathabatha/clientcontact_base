b9ed116205441cd257011ac492d081a5e7275017
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-15 02:42:36 +0200

    add back to clients btn

--- client/src/components/Clients/ClientsDetails.jsx
+++ client/src/components/Clients/ClientsDetails.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import ClientsGeneralTab from '../Clients/ClientsUpdateTabs/ClientsGeneralTab';
 import ClientsContactsTab from '../Clients/ClientsUpdateTabs/ClientsContactsTab';
+import { Link } from 'react-router-dom'
 
 export default function ClientDetails() {
   const [activeTab, setActiveTab] = useState('general');
 
   return (
     <div>
+      <Link to='/clients' className='btn btn-success'>Back to Clients</Link>
+
       <button onClick={() => setActiveTab('general')}>General</button>
       <button onClick={() => setActiveTab('contacts')}>Contacts</button>
 
