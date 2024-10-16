f43b54de2b53546439e00670cec4ff4eb3e3ebc3
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-10-16 11:26:52 +0200

    remove edit functionality

--- client/src/components/Contacts/ContactsUpdateTabs/ContactsGeneralTab.jsx
+++ client/src/components/Contacts/ContactsUpdateTabs/ContactsGeneralTab.jsx
@@ -4,23 +4,11 @@ import axios from 'axios';
 
 export default function ContactsGeneralTab() {
   const { id } = useParams(); 
-  // const [contactName, setContactName] = useState('');
-  // const [contactSurname, setContactSurname] = useState('');
-  // const [contactEmail, setContactEmail] = useState('');
-  // const [originalContactName, setOriginalContactName] = useState('');
-  // get entire data set from contact
   const [contactData, setContactData] = useState({
     name: '',
     contact_surname: '',
     email: ''
-  });
-
-  const [originalContactData, setOriginalContactData] = useState({
-    name: '',
-    contact_surname: '',
-    email: ''
-  });
-  
+  }); 
 
 useEffect(() => {
   console.log(`first ${id}`)
@@ -28,10 +16,8 @@ useEffect(() => {
     console.log(`second ${id}`)
     axios.get(`http://localhost:5000/api/contacts/${id}`)
       .then((response) => {
-        const contactData = response.data;
-        console.log(contactData);
-        setContactData(contactData.name)
-            
+        const contact = response.data[0];
+        setContactData(contact);        
       })
       .catch((error) => {
         console.error("There was an error fetching the contact data!", error);
@@ -40,7 +26,8 @@ useEffect(() => {
 }, [id]);
 
 
-  const handleSave = () => {
+// function to save updates to existing contact entry
+/*  const handleSave = () => {
     if (contactData.name !== originalContactData.name) {
       axios.put(`http://localhost:5000/api/contacts/${id}`, { name: contactData.name })
         .then(() => {
@@ -51,44 +38,47 @@ useEffect(() => {
           console.error("There was an error updating the contact name!", error);
         });
     }
-  };
+  }; */
 
   return (
     <div>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Contact Name:</label>
+        <label htmlFor="name" className="form-label">Contact Name</label>
         <input
           type="text"
           className="form-control"
           id="name"
           value={contactData.name}
           placeholder={contactData.name}
-          onChange={(e) => setContactData.name(e.target.value)}
+          readonly
+          // onChange={(e) => setContactData.name(e.target.value)}
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="contact_surname" className="form-label">Contact Surname:</label>
+        <label htmlFor="contact_surname" className="form-label">Contact Surname</label>
         <input
           type="text"
           className="form-control"
           id="contact_surname"
-          value={contactData.surname}
-          placeholder='test'
-          onChange={(e) => setContactData.contact_surname(e.target.value)}
+          value={contactData.contact_surname}
+          placeholder={contactData.contact_surname}
+          readonly
+          // onChange={(e) => setContactData.contact_surname(e.target.value)}
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="email" className="form-label">Contact Email:</label>
+        <label htmlFor="email" className="form-label">Contact Email</label>
         <input
           type="email"
           className="form-control"
           id="email"
           value={contactData.email}
-          placeholder='test'
-          onChange={(e) => setContactData.email(e.target.value)}
+          placeholder={contactData.email}
+          readonly
+          // onChange={(e) => setContactData.email(e.target.value)}
         />
       </div>
-      <button className="btn btn-primary" onClick={handleSave}>Save</button>
+      {/* <button className="btn btn-primary" onClick={handleSave}>Save</button> */}
     </div>
   );
 }
